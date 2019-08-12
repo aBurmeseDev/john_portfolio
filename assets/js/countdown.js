9c7eb9af2bced00e712af9e6fa09db51e584e63a
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-11 22:45:31 -0700

    get current date and set interval

---
+++ assets/js/countdown.js
@@ -0,0 +1,10 @@
+// set the date that's counting down
+const countDownDate = new Date('August 31, 2019 15:30:25');
+
+// update the count down every 1 second
+const x = setInterval(function() {
+  // get today's date and tiem
+  const now = new Data().getTime();
+  // find the distance between now and the count down date
+  const distance = countDownDate - now;
+});
