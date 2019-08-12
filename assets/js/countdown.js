54ffcda299df2207debfed5b33140c7772201a34
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-12 00:12:32 -0700

    countdown running

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,18 +1,20 @@
 // set the date that's counting down
-const countDownDate = new Date('August 31, 2019 15:30:25');
+let countDownDate = new Date('Aug 31, 2019 15:30:25').getTime();
 
 // update the count down every 1 second
-const x = setInterval(function() {
+let x = setInterval(function() {
   // get today's date and tiem
-  const now = new Data().getTime();
+  let now = new Date().getTime();
   // find the distance between now and the count down date
-  const distance = countDownDate - now;
+  let distance = countDownDate - now;
 
   // time calculations for days, hours, minutes and seconds
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // display the result in the element with id-memo
+  document.getElementById('demo').innerHTML =
+    days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
 });
